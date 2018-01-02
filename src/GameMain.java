dcd4dc2310e29516a5aa192d941cda55bc473c5d
XYFFFighting <765044145@qq.com>
2018-01-01 22:11:37 -0500

    Update GameMain.java

--- src/GameMain.java
+++ src/GameMain.java
@@ -1,3 +1,23 @@
-public class GameMain {
+import chess.Board;
+import control.ChessController;
+import view.BoardView;
 
+public class GameMain {
+    private Board board;
+    private ChessController controller;
+    private BoardView view;
+
+    public void set(){
+        controller = new ChessController();
+        board = controller.init();
+        view = new BoardView(controller);
+        view.init(board);
+    }
+
+
+
+    public static void main(String[] args) {
+        GameMain main = new GameMain();
+        main.set();
+    }
 }
