0f5e723ebc939f9c0d798349f0d6cb1956587299
XYFFFighting <765044145@qq.com>
2018-01-01 22:48:17 -0500

    Add files via upload

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -6,7 +6,7 @@ public class Board {
     public int BOARD_WIDTH=200;//200
     public int BOARD_HEIGHT=100;//100
     public Map<String, Piece> pieceMap;
-    private Piece[][] chessBoard = new Piece[BOARD_HEIGHT][BOARD_WIDTH];
+    private Piece[][] chessBoard = new Piece[BOARD_WIDTH][BOARD_HEIGHT];
 
     public boolean isInside(int x, int y){
         if(x<0||x>=BOARD_HEIGHT||y<0||y>=BOARD_WIDTH)
--- src/control/ChessController.java
+++ src/control/ChessController.java
@@ -11,17 +11,17 @@ public class ChessController {
         Map<String, Piece> pieceMap = new HashMap<String, Piece>();
         /*add Pieces here*/
         pieceMap.put("R1", new Piece("R1", new int[]{0,0}));
-        pieceMap.put("R3", new Piece("R3", new int[]{19,0}));
-        pieceMap.put("R5", new Piece("R5", new int[]{39,0}));
-        pieceMap.put("R7", new Piece("R7", new int[]{59,0}));
-        pieceMap.put("R9", new Piece("R9", new int[]{79,0}));
-        pieceMap.put("R11", new Piece("R11", new int[]{99,0}));
-        pieceMap.put("B2", new Piece("B2", new int[]{0,199}));
-        pieceMap.put("B4", new Piece("B4", new int[]{19,199}));
-        pieceMap.put("B6", new Piece("B6", new int[]{39,199}));
-        pieceMap.put("B8", new Piece("B8", new int[]{59,199}));
-        pieceMap.put("R10", new Piece("R10", new int[]{79,199}));
-        pieceMap.put("R12", new Piece("R12", new int[]{99,199}));
+        pieceMap.put("R3", new Piece("R3", new int[]{0,19}));
+        pieceMap.put("R5", new Piece("R5", new int[]{0,39}));
+        pieceMap.put("R7", new Piece("R7", new int[]{0,59}));
+        pieceMap.put("R9", new Piece("R9", new int[]{0,79}));
+        pieceMap.put("R11", new Piece("R11", new int[]{0,99}));
+        pieceMap.put("B2", new Piece("B2", new int[]{199,0}));
+        pieceMap.put("B4", new Piece("B4", new int[]{199,19}));
+        pieceMap.put("B6", new Piece("B6", new int[]{199,39}));
+        pieceMap.put("B8", new Piece("B8", new int[]{199,59}));
+        pieceMap.put("B10", new Piece("B10", new int[]{199,79}));
+        pieceMap.put("B12", new Piece("B12", new int[]{199,99}));
         return pieceMap;
 
     }
